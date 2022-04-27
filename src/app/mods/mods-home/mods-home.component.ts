ef7fa31ff844e37f5d912b4905082a59d9d6f6d9
Paolo <pieffelleo@gmail.com>
2022-04-27 14:39:18 +0200

    mod and accordion section end

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mods-home',
   templateUrl: './mods-home.component.html',
-  styleUrls: ['./mods-home.component.css']
+  styleUrls: ['./mods-home.component.css'],
 })
 export class ModsHomeComponent implements OnInit {
+  modalOpen = false;
+  items = [
+    { title: 'Is this angular', content: 'of course!' },
+    { title: 'With Typescript', content: 'you can type on it!' },
+    { title: 'In javascript', content: 'what else!' },
+  ];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onClick() {
+    this.modalOpen = !this.modalOpen;
   }
-
 }
